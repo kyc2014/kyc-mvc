a63f4d87e0e29d79c15cfb541a9d2ea131060e27
maxsteal <maxsteal@maxsteal-Vostro-1550.(none)>
2013-10-27 23:37:28 +0530

    Removed everything

--- src/main/java/net/knowyourcandidate/spring/web/candidate/dao/CandidateDao.java
+++
@@ -1,5 +0,0 @@
-package net.knowyourcandidate.spring.web.candidate.dao;
-
-public interface CandidateDao {
-	public Object retrieveCandidate(String name);
-}
--- src/main/java/net/knowyourcandidate/spring/web/candidate/service/CandidateService.java
+++
@@ -1,7 +0,0 @@
-package net.knowyourcandidate.spring.web.candidate.service;
-
-import net.knowyourcandidate.spring.web.candidate.model.Candidate;
-
-public interface CandidateService {
-	public Candidate retrieveCandidateByName(String name);
-}
