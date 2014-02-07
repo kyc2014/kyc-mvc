71b5f007ac9af82e029a03d3b9394e6e2fced35d
sathyankumar <sathyan.malar@gmail.com>
2014-02-07 23:32:03 +0530

    Added Rajasthan images

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 mvn clean package
-cp /home/ganeshprabhu/knowyourcandidate/codebase/kyc-mvc/target/kyc-mvc.war /home/ganeshprabhu/apache-tomcat-7.0.42/webapps/
-tail -f /home/ganeshprabhu/apache-tomcat-7.0.42/logs/catalina.out
+cp ~/knowyourcandidate/codebase/kyc-mvc/target/kyc-mvc.war ~/apache-tomcat-7.0.42/webapps/
+tail -f ~/apache-tomcat-7.0.42/logs/catalina.out
